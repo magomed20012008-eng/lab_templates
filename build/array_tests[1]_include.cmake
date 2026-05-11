7fbf3465a8030a2c985a83cb6524214a498e8d00
Muhammad <magomed20012008@gmail.com>
2026-05-12 00:53:30 +0300

    Array is done

---
+++ build/array_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/mnt/c/Users/Redmi/It-Projects/lab02/lab_templates/build/array_tests[1]_tests.cmake")
+  include("/mnt/c/Users/Redmi/It-Projects/lab02/lab_templates/build/array_tests[1]_tests.cmake")
+else()
+  add_test(array_tests_NOT_BUILT array_tests_NOT_BUILT)
+endif()
---
+++ build/unique_ptr_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/mnt/c/Users/Redmi/It-Projects/lab02/lab_templates/build/unique_ptr_tests[1]_tests.cmake")
+  include("/mnt/c/Users/Redmi/It-Projects/lab02/lab_templates/build/unique_ptr_tests[1]_tests.cmake")
+else()
+  add_test(unique_ptr_tests_NOT_BUILT unique_ptr_tests_NOT_BUILT)
+endif()
